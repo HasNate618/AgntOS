4449f95ef22e9fe1893894bb9c897f775cb2a096
HasNate618 <nate.e.espejo@gmail.com>
2026-05-14 23:33:25 -0400

    fix: use sddm.settings.Theme.background for SDDM wallpaper

--- modules/agntos/desktop-plasma.nix
+++ modules/agntos/desktop-plasma.nix
@@ -2,20 +2,15 @@
 
 let
   wallpaperPath = "${pkgs.agntos-branding}/share/wallpapers/agntos/default.png";
-
-  # Override Breeze SDDM theme with our wallpaper
-  sddmBreezeWithBackground = pkgs.runCommand "sddm-breeze-agntos" {} ''
-    mkdir -p $out/share/sddm/themes/breeze
-    cp -r ${pkgs.plasma-desktop}/share/sddm/themes/breeze/* $out/share/sddm/themes/breeze/
-    sed -i "s|background=.*|background=${wallpaperPath}|" $out/share/sddm/themes/breeze/theme.conf
-  '';
-
 in {
   config = lib.mkIf config.agntos.enable {
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      theme = sddmBreezeWithBackground;
+      settings.Theme = {
+        background = wallpaperPath;
+        type = "image";
+      };
     };
     services.desktopManager.plasma6.enable = true;
     services.xserver.enable = true;
