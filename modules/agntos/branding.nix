9285c28036c20d6b8715aeab3d1bebe7d62d9878
HasNate618 <nate.e.espejo@gmail.com>
2026-05-16 11:27:14 -0400

    fix: use hardcoded orange for Klassy window outlines
    - Bart color scheme accent not picked up by Klassy
    - Switch to WindowOutlineCustomColor with #F57C48 (245,124,72)
    - Set opacity: 100% active, 60% inactive
    - CustomWithContrast at 80%/50% for better visibility

--- modules/agntos/branding.nix
+++ modules/agntos/branding.nix
@@ -99,9 +99,15 @@ in {
     [Windeco]
     WindowCornerRadius=8
     RoundAllCornersWhenNoBorders=true
-    ThinWindowOutlineStyleActive=WindowOutlineAccentColor
-    ThinWindowOutlineStyleInactive=WindowOutlineAccentWithContrast
+    ThinWindowOutlineStyleActive=WindowOutlineCustomColor
+    ThinWindowOutlineStyleInactive=WindowOutlineCustomWithContrast
+    ThinWindowOutlineCustomColorActive=245,124,72
+    ThinWindowOutlineCustomColorInactive=245,124,72
     ThinWindowOutlineThickness=1.75
+    WindowOutlineCustomColorOpacityActive=100
+    WindowOutlineCustomColorOpacityInactive=60
+    WindowOutlineCustomWithContrastOpacityActive=80
+    WindowOutlineCustomWithContrastOpacityInactive=50
     ShowOutlineNormallyActive=true
     ShowOutlineNormallyInactive=true
     ShowCloseOutlineNormallyActive=true
